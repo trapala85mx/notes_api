2fb75e9f62eb3ab8ddde544bfc1e0a3e36648154
trapala85mx <rtrapala.dev.ds@gmail.com>
2025-05-06 01:08:09 -0600

    [ADD] register Note to the DjangAdmin with notes.admin.NoteAdmin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,29 @@
+"""notes/admin.py"""
+
 from django.contrib import admin
 
+from .models import Note
+
+
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    """Configuración para el modelo Note en el Admin."""
+
+    list_display = (
+        "title",
+        "content",
+        "created_at",
+    )
+    list_filter = (
+        "title",
+        "created_at",
+    )
+    ordering = (
+        "title",
+        "created_at",
+    )
+
+
+# Le decimos a Django que registe el modelo Note
+# con la configuración de NoteAdmin
+admin.site.register(Note, NoteAdmin)
